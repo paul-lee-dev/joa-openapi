e15d633f11556433742e08f8d7d65edbb52802a9
이유로 <eurohand@naver.com>
2024-03-14 15:25:40 +0900

    S10P12A506-187 Feat: set up nativewind

--- bank/frontend/ios/frontendTests/frontendTests.m
+++ bank/frontend/ios/frontendTests/frontendTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface joabankappTests : XCTestCase
+@interface frontendTests : XCTestCase
 
 @end
 
-@implementation joabankappTests
+@implementation frontendTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
