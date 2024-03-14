372dd5ceb1759b6293909c720d773766e51ad5d4
이유로 <eurohand@naver.com>
2024-03-14 17:03:55 +0900

    S10P12A506-187 Feat: common components

--- bank/frontend/src/Template.tsx
+++ bank/frontend/src/Template.tsx
@@ -1,10 +1,18 @@
 import {View} from 'react-native';
 import Header from './Header';
+import CommonInput from './CommonInput';
+import CommonMenuItem from './CommonMenuItem';
 
 function Template(): React.JSX.Element {
   return (
     <View className="w-full h-full">
       <Header />
+      <CommonInput label={'이메일'} />
+      <CommonInput label={'이름'} />
+      <CommonInput label={'비밀번호'} />
+      <CommonMenuItem title={'알림설정'} />
+      <CommonMenuItem title={'앱 환경설정'} />
+      <CommonMenuItem title={'은행코드 변경'} />
     </View>
   );
 }
