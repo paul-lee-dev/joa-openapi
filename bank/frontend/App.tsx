25642a3586948d23963a66ab7ca48d20f875b6bb
이유로 <eurohand@naver.com>
2024-03-18 15:32:03 +0900

    S10P12A506-239 Feat: transfer amount page

--- bank/frontend/App.tsx
+++ bank/frontend/App.tsx
@@ -3,13 +3,21 @@ import Intro from './src/pages/Intro';
 import Join from './src/pages/Join';
 import Main from './src/pages/Main';
 import Splash from './src/pages/Splash';
+import Transfer from './src/pages/Transfer';
+import TransferAmount from './src/pages/TransferAmount';
+import TransferConfirm from './src/pages/TransferConfirm';
+import TransferResult from './src/pages/TransferResult';
 
 function App(): React.JSX.Element {
   // return <Splash />;
   // return <Main />;
   // return <Intro />;
   // return <Join />;
-  return <History />;
+  // return <History />;
+  // return <Transfer />;
+  // return <TransferAmount />;
+  // return <TransferConfirm />;
+  return <TransferResult />;
 }
 
 export default App;
