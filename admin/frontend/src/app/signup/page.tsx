0da53aa49cbd62c8f570f1ce0bff37b952388c3e
paul <cebu13@gmail.com>
2024-03-18 13:19:45 +0900

    S10P12A506-196 feat: signup page phone check modal layout

--- admin/frontend/src/app/signup/page.tsx
+++ admin/frontend/src/app/signup/page.tsx
@@ -1,6 +1,28 @@
-import tw from "tailwind-styled-components";
+"use client";
 
-export default function Home() {
+import { useRecoilState } from "recoil";
+import tw from "tailwind-styled-components";
+import { ModalState, VerificationState } from "./state";
+
+export default function SignUp() {
+  const [isModalOpen, setIsModalOpen] = useRecoilState(ModalState);
+  const [verificationCode, setVerificationCode] =
+    useRecoilState(VerificationState);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  const handleVerificationCodeChange = (event: {
+    target: { value: string | ((currVal: string) => string) };
+  }) => {
+    setVerificationCode(event.target.value);
+  };
+
+  const handleSubmitVerificationCode = () => {
+    setIsModalOpen(!isModalOpen);
+    console.log("Verifying code:", verificationCode);
+  };
   return (
     <>
       <Wrapper>
@@ -21,7 +43,7 @@ export default function Home() {
 
             <InputFormWrapper>
               <LoginLabel htmlFor="tel">전화번호</LoginLabel>
-              <InputContainer>
+              <InputContainerWithButton>
                 <Input
                   id="tel"
                   name="tel"
@@ -29,7 +51,10 @@ export default function Home() {
                   autoComplete="tel"
                   required
                 />
-              </InputContainer>
+                <PhoneCallCheckBtn type="button" onClick={toggleModal}>
+                  확인
+                </PhoneCallCheckBtn>
+              </InputContainerWithButton>
             </InputFormWrapper>
 
             <InputFormWrapper>
@@ -57,13 +82,33 @@ export default function Home() {
                 />
               </InputContainer>
             </InputFormWrapper>
-
             <InputFormWrapper>
               <Button type="submit">회원가입</Button>
             </InputFormWrapper>
           </LoginFormContainer>
         </MainContainer>
       </Wrapper>
+      {isModalOpen && (
+        <Modal>
+          <ModalContent>
+            <h1>인증번호 확인</h1>
+            <InputContainerWithButton>
+              <Input
+                id="verificationCode"
+                name="verificationCode"
+                type="text"
+                value={verificationCode}
+                onChange={handleVerificationCodeChange}
+                placeholder="인증번호 입력"
+                required
+              />
+              <PhoneCallCheckBtn onClick={handleSubmitVerificationCode}>
+                확인
+              </PhoneCallCheckBtn>
+            </InputContainerWithButton>
+          </ModalContent>
+        </Modal>
+      )}
     </>
   );
 }
@@ -90,6 +135,13 @@ const InputFormWrapper = tw.div`
 
 const InputContainer = tw.div`
 mt-2
+`;
+
+const InputContainerWithButton = tw.div`
+  mt-2
+  flex
+  items-center
+  space-x-4
 `;
 
 const LoginFormContainer = tw.form`
@@ -122,12 +174,12 @@ sm:leading-6
 
 const Button = tw.button`
 flex
-w-full 
 justify-center 
-rounded-md 
+rounded-md
+w-full
 bg-pink-400
 px-3 
-py-1.5 
+py-1.5
 text-sm 
 font-semibold
 leading-6
@@ -138,4 +190,47 @@ focus-visible:outline
 focus-visible:outline-2
 focus-visible:outline-offset-2 
 focus-visible:outline-pink-600
+`;
+
+const PhoneCallCheckBtn = tw.button`
+  flex
+  justify-center
+  w-fit
+  rounded-md 
+  bg-pink-400
+  px-3
+  py-1.5
+  text-xs
+  font-semibold
+  leading-6
+  text-white
+  shadow-sm
+  hover:bg-pink-500
+  focus-visible:outline
+  focus-visible:outline-2
+  focus-visible:outline-offset-2
+  focus-visible:outline-pink-600
+  whitespace-nowrap
+`;
+
+const Modal = tw.div`
+  fixed
+  top-0
+  left-0
+  w-full
+  h-full
+  flex
+  items-center
+  justify-center
+  bg-gray-700
+  bg-opacity-75
+  z-50
+`;
+
+const ModalContent = tw.div`
+  bg-white
+  rounded-lg
+  p-8
+  max-w-md
+  w-full
 `;
