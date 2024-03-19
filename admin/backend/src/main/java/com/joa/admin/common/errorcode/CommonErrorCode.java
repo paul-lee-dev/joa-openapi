17e5b7952ebc8b3a4ac1bfb0ba3eec04efe2cc97
조아영 <aystarr@naver.com>
2024-03-19 16:08:49 +0900

    modify: CommonErrorCode에 Enter 추가

--- admin/backend/src/main/java/com/joa/admin/common/errorcode/CommonErrorCode.java
+++ admin/backend/src/main/java/com/joa/admin/common/errorcode/CommonErrorCode.java
@@ -12,6 +12,7 @@ public enum CommonErrorCode implements ErrorCode{
     WRONG_REQUEST(HttpStatus.BAD_REQUEST, "요청이 잘못되었습니다"),
     RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 리소스가 존재하지 않습니다"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러입니다");
+    
     private final HttpStatus httpStatus;
     private final String message;
 }
