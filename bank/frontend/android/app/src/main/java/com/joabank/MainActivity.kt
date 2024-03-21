9e424e9895ae1d29b70690a723d350f0ad7b21e9
이유로 <eurohand@naver.com>
2024-03-21 10:05:29 +0900

    S10P12A506-261 modify: rename app

--- bank/frontend/android/app/src/main/java/com/joabank/MainActivity.kt
+++ bank/frontend/android/app/src/main/java/com/joabank/MainActivity.kt
@@ -1,10 +1,9 @@
-package com.frontend
+package com.joabank
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-
 import android.os.Bundle;
 
 class MainActivity : ReactActivity() {
@@ -13,7 +12,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "frontend"
+  override fun getMainComponentName(): String = "joabank"
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(null)
