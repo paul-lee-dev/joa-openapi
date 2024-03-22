5a5722f1f82d12f5feea3ef7abd86c1c6242a15d
bonseung <rnqhstmd0219@gmail.com>
2024-03-22 09:13:30 +0900

    S10P12A506-235 conflict: 머지충돌해결

---
+++ openapi/backend/src/main/java/com/joa/openapi/account/dto/AccountGetBalanceResponseDto.java
@@ -0,0 +1,26 @@
+package com.joa.openapi.account.dto;
+
+import com.joa.openapi.account.entity.Account;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountGetBalanceResponseDto {
+
+    private String accountId;
+    private String nickname;
+    private Long balance;
+
+    public static AccountGetBalanceResponseDto toDto(Account account) {
+        return AccountGetBalanceResponseDto.builder()
+                .accountId(account.getId())
+                .nickname(account.getNickname())
+                .balance(account.getBalance())
+                .build();
+    }
+}
