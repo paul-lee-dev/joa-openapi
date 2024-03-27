059f0f406419b84309032a950ae041d919dc6ec2
KohSulim <sulim0314@naver.com>
2024-03-27 16:09:16 +0900

    S10P12A506-265 chore : admin dashboard 배포 확인

--- admin/frontend/next.config.mjs
+++ admin/frontend/next.config.mjs
@@ -4,6 +4,15 @@
 
 const nextConfig = {
   basePath: "/dashboard",
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: `http://127.0.0.1:3000/:path*`,
+        basePath: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
