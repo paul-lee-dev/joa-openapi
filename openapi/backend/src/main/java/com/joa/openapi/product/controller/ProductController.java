a4858358ddaa3a9082ca9c779127291830e6400b
고수림 <sulim0314@naver.com>
2024-03-28 01:02:42 +0900

    309 WIP : 예적금 상품 조회 구현중

--- openapi/backend/src/main/java/com/joa/openapi/product/controller/ProductController.java
+++ openapi/backend/src/main/java/com/joa/openapi/product/controller/ProductController.java
@@ -1,16 +1,25 @@
 package com.joa.openapi.product.controller;
 
-import com.joa.openapi.account.dto.*;
 import com.joa.openapi.common.response.ApiResponse;
 import com.joa.openapi.product.dto.ProductCreateRequestDto;
 import com.joa.openapi.product.dto.ProductCreateResponseDto;
 import com.joa.openapi.product.dto.ProductUpdateIsDoneResponseDto;
 import com.joa.openapi.product.service.ProductService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
+import com.joa.openapi.product.dto.ProductSearchResponseDto;
 import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/product")
@@ -36,4 +45,12 @@ public class ProductController {
         productService.delete(productId);
         return ResponseEntity.ok(ApiResponse.success("계좌 해지에 성공했습니다."));
     }
+
+    // queryDsl로 예적금 상품 조회
+    @GetMapping("/search")
+    public ResponseEntity<?> search(@PageableDefault Pageable pageable) {
+        Page<ProductSearchResponseDto> productsPage = productService.search(pageable);
+        return ResponseEntity.ok(ApiResponse.success("예적금 상품 조회에 성공했습니다.", productsPage));
+    }
+
 }
--- openapi/backend/src/main/java/com/joa/openapi/product/service/ProductService.java
+++ openapi/backend/src/main/java/com/joa/openapi/product/service/ProductService.java
@@ -4,16 +4,18 @@ import com.joa.openapi.account.entity.Account;
 import com.joa.openapi.common.exception.RestApiException;
 import com.joa.openapi.product.dto.ProductCreateRequestDto;
 import com.joa.openapi.product.dto.ProductCreateResponseDto;
+import com.joa.openapi.product.dto.ProductSearchResponseDto;
 import com.joa.openapi.product.dto.ProductUpdateIsDoneResponseDto;
 import com.joa.openapi.product.entity.Product;
 import com.joa.openapi.product.errorcode.ProductErrorCode;
 import com.joa.openapi.product.repository.ProductRepository;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.UUID;
 
 @Slf4j
 @Service
@@ -29,15 +31,15 @@ public class ProductService {
         //authorityValidation(memberId); /* TODO 관리자 권한 설정 */
 
         Product product = Product.builder()
-                .name(req.getName())
-                .description(req.getDescription())
-                .minAmount(req.getMinAmount())
-                .maxAmount(req.getMaxAmount())
-                .rate(req.getRate())
-                .productType(req.getProductType())
-                .paymentType(req.getPaymentType())
-                .isDone(false)
-                .build();
+            .name(req.getName())
+            .description(req.getDescription())
+            .minAmount(req.getMinAmount())
+            .maxAmount(req.getMaxAmount())
+            .rate(req.getRate())
+            .productType(req.getProductType())
+            .paymentType(req.getPaymentType())
+            .isDone(false)
+            .build();
 
         productRepository.save(product);
 
@@ -49,10 +51,12 @@ public class ProductService {
 
         //authorityValidation(memberId);
 
-        Product product = productRepository.findById(productId).orElseThrow(() -> new RestApiException(ProductErrorCode.NO_PRODUCT));
+        Product product = productRepository.findById(productId)
+            .orElseThrow(() -> new RestApiException(ProductErrorCode.NO_PRODUCT));
 
-        if(product.getIsDone())
+        if (product.getIsDone()) {
             throw new RestApiException(ProductErrorCode.ALREADY_DONE);
+        }
         product.updateIsDone(true);
 
         productRepository.save(product);
@@ -65,17 +69,25 @@ public class ProductService {
 
         //authorityValidation(memberId);
 
-        Product product = productRepository.findById(productId).orElseThrow(() -> new RestApiException(ProductErrorCode.NO_PRODUCT));
+        Product product = productRepository.findById(productId)
+            .orElseThrow(() -> new RestApiException(ProductErrorCode.NO_PRODUCT));
 
-        boolean allAccountsDeleted = product.getAccountList().stream().allMatch(Account::getIsDeleted);
+        boolean allAccountsDeleted = product.getAccountList().stream()
+            .allMatch(Account::getIsDeleted);
 
-        if(allAccountsDeleted)
+        if (allAccountsDeleted) {
             product.deleteSoftly();
-        else
+        } else {
             throw new RestApiException(ProductErrorCode.NO_PRODUCT);
+        }
     }
 
     public void authorityValidation(UUID memberId) {
 
     }
+
+    public Page<ProductSearchResponseDto> search(Pageable pageable) {
+        return productRepository.searchProductCustom(pageable);
+    }
+
 }
