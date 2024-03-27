8dd919dcf0959eeb686543c49a103fc71ba56922
paul <cebu13@gmail.com>
2024-03-27 13:01:01 +0900

    S10P22A503-284 fix: dummy member api

--- admin/frontend/next.config.mjs
+++ admin/frontend/next.config.mjs
@@ -2,15 +2,22 @@
 const nextConfig = {
   distDir: "build",
   output: "export",
-  async rewrites() {
-    //CORS 문제 해결을 위한 설정(proxy 설정)
-    return [
-      {
-        source: "/:path*",
-        destination: `http://joa13.site:path*`, //컨플루언스의 API주소
-      },
-    ];
-  },
+  // generateBuildId: async () => {
+  //   // This could be anything, using the latest git hash
+  //   return "";
+  // },
+  // env: {
+  //   customKey: "my-value",
+  // },
+  // async rewrites() {
+  //   //CORS 문제 해결을 위한 설정(proxy 설정)
+  //   return [
+  //     {
+  //       source: "/:path*",
+  //       destination: `http://joa13.site:path*`, //컨플루언스의 API주소
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
