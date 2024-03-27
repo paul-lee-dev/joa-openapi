d66a69e3d7ea39d8573466670412626bc409a424
조아영 <aycho3030@gmail.com>
2024-03-27 17:14:22 +0900

    S10P22A503-330 feat: 은행 고객 로그인/로그아웃

--- bank/backend/src/main/java/com/joa/bank/member/dto/MemberLoginRequestDto.java
+++ bank/backend/src/main/java/com/joa/bank/member/dto/MemberLoginRequestDto.java
@@ -1,10 +1,13 @@
 package com.joa.bank.member.dto;
 
+import lombok.Getter;
+
 import java.util.UUID;
 
+@Getter
 public class MemberLoginRequestDto {
 
-    private UUID id;
+    private String email;
     private String password;
     private Boolean isDeleted;
 }
--- bank/backend/src/main/java/com/joa/bank/member/dto/MemberResponseDto.java
+++
@@ -1,8 +0,0 @@
-package com.joa.bank.member.dto;
-
-import java.util.UUID;
-
-public class MemberResponseDto {
-
-
-}
