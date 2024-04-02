7d3135b1b67bd97e12e0a41bc19d201ad7b76be5
이유로 <eurohand@naver.com>
2024-04-02 12:25:28 +0900

    S10P22A503-378 feat: admin login redirect

--- admin/frontend/src/models/Admin.interface.ts
+++ admin/frontend/src/models/Admin.interface.ts
@@ -4,3 +4,17 @@ export interface CreateAdminParams {
   phone: string;
   password: string;
 }
+
+export interface IAdminData {
+  isLogin: boolean;
+  apiKey: string | null;
+  accessToken: string | null;
+  refreshToken: string | null;
+}
+
+export const defaultAdminData: IAdminData = {
+  isLogin: false,
+  apiKey: null,
+  accessToken: null,
+  refreshToken: null,
+};
--- admin/frontend/src/store/atom.ts
+++ admin/frontend/src/store/atom.ts
@@ -1,27 +1,13 @@
+import { IAdminData, defaultAdminData } from "@/models/Admin.interface";
 import { atom } from "recoil";
 import { recoilPersist } from "recoil-persist";
 
-const sessionStorage =
-  typeof window !== "undefined" ? window.sessionStorage : undefined;
+const sessionStorage = typeof window !== "undefined" ? window.sessionStorage : undefined;
 
 const { persistAtom } = recoilPersist({
   key: "persistAtom",
   storage: sessionStorage,
 });
-
-interface IAdminData {
-  isLogin: boolean;
-  apiKey: string | null;
-  accessToken: string | null;
-  refreshToken: string | null;
-}
-
-const defaultAdminData: IAdminData = {
-  isLogin: false,
-  apiKey: null,
-  accessToken: null,
-  refreshToken: null,
-};
 
 export const adminDataAtom = atom<IAdminData>({
   key: "adminData",
