cab76b981fe4dfee3f83e738df5be92fca7929c1
paul <cebu13@gmail.com>
2024-04-04 17:57:10 +0900

    feat: trransaction date range search

--- admin/frontend/src/app/admin/transaction/page.tsx
+++ admin/frontend/src/app/admin/transaction/page.tsx
@@ -4,13 +4,18 @@ import { searchTransactionList } from "@/api/Transaction";
 import Button from "@/components/button/button";
 import { LoadingSpinner } from "@/components/loadingSpinner";
 import Pagination from "@/components/pagination";
-import AccountMultiSearchSelect from "@/components/select/accountMultiSearchSelect";
-import BankMultiSearchSelect from "@/components/select/bankMultiSearchSelect";
-import MemberMultiSearch from "@/components/select/memberMultiSearch";
 import TransactionTable from "@/components/table/transactionTable";
 import { useQuery } from "@tanstack/react-query";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "@/styles/datepicker.css";
+import { registerLocale, setDefaultLocale } from "react-datepicker";
+import { ko } from "date-fns/locale";
+import { format } from "date-fns";
+
+registerLocale("ko", ko);
 
 const Dashboard = () => {
   const router = useRouter();
@@ -22,10 +27,30 @@ const Dashboard = () => {
   const [searchWord, setSearchWord] = useState<string>(
     params.get("depositorNameKeyword") ?? ""
   );
+
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
+
+  const [fromDate, setFromDate] = useState<string>(
+    params.get("fromDate") ?? ""
+  );
+  const [toDate, setToDate] = useState<string>(params.get("toDate") ?? "");
+
   const { data, isLoading, refetch } = useQuery({
-    queryKey: ["TransactionList", searchWord || "all", page],
+    queryKey: [
+      "TransactionList",
+      searchWord || "all",
+      page,
+      fromDate || "all",
+      toDate || "all",
+    ],
     queryFn: () => {
-      return searchTransactionList({ depositorNameKeyword: searchWord, page });
+      return searchTransactionList({
+        depositorNameKeyword: searchWord,
+        page,
+        fromDate: fromDate || null,
+        toDate: toDate || null,
+      });
     },
   });
 
@@ -41,10 +66,15 @@ const Dashboard = () => {
     if (searchWord !== "") {
       newParams.set("depositorNameKeyword", searchWord);
     }
+    if (fromDate !== null) {
+      newParams.set("fromDate", fromDate);
+    }
+    if (toDate !== "") {
+      newParams.set("toDate", toDate);
+    }
     console.log(newParams);
     router.replace(`/admin/transaction?${newParams.toString()}`);
-  }, [router, page, searchWord]);
-
+  }, [router, page, searchWord, fromDate, toDate]);
   return (
     <>
       {isLoading ? (
@@ -59,8 +89,54 @@ const Dashboard = () => {
                 e.preventDefault();
                 setPage(1);
                 setSearchWord(keyword);
+                setFromDate(fromDate);
+                setToDate(toDate);
               }}
             >
+              <div className="w-52 h-10">
+                <DatePicker
+                  locale={ko}
+                  className="rounded-md border-0 py-1.5 px-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholderText="시작일자"
+                  minDate={new Date("2023-06-02")}
+                  maxDate={new Date()}
+                  dateFormat="yyyy-MM-dd"
+                  selected={startDate}
+                  onChange={(startDate) => {
+                    setStartDate(startDate);
+                    if (startDate !== null) {
+                      setFromDate(
+                        format(
+                          new Date(startDate?.toLocaleDateString()),
+                          "yyyy-MM-dd"
+                        )
+                      );
+                    }
+                  }}
+                ></DatePicker>
+              </div>
+              <div className="w-52 h-10">
+                <DatePicker
+                  locale={ko}
+                  className="rounded-md border-0 py-1.5 px-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholderText="종료일자"
+                  minDate={new Date("2023-06-02")}
+                  maxDate={new Date()}
+                  dateFormat="yyyy-MM-dd"
+                  selected={endDate}
+                  onChange={(endDate) => {
+                    setEndDate(endDate);
+                    if (endDate !== null) {
+                      setToDate(
+                        format(
+                          new Date(endDate?.toLocaleDateString()),
+                          "yyyy-MM-dd"
+                        )
+                      );
+                    }
+                  }}
+                ></DatePicker>
+              </div>
               <div className="w-52 h-10">
                 <input
                   type="text"
@@ -71,6 +147,7 @@ const Dashboard = () => {
                   placeholder="입금자 명"
                 />
               </div>
+
               <Button id={"search"} name={"검색"}></Button>
             </form>
           </div>
